1bb485d2c75a5cb08bd9b2366f6c266d267fc628
UnLovedCookie <107450640+UnLovedCookie@users.noreply.github.com>
2022-06-15 20:00:24 -0700

    Add Auraside
    Add Auraside

--- HoneCtrl.bat
+++ HoneCtrl.bat
@@ -1,4 +1,4 @@
-::    Copyright (C) <year>  <name of author>
+::    Copyright (C) 2022 Auraside
 ::
 ::    This program is free software: you can redistribute it and/or modify
 ::    it under the terms of the GNU Affero General Public License as published
